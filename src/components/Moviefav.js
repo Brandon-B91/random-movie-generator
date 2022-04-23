b8a3f5da180bf8655b5a6dfd4c90fd0bc778479b
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2022-04-23 14:15:46 -0600

    added clear movies and clear tv options

--- src/components/Moviefav.js
+++ src/components/Moviefav.js
@@ -1,30 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const Moviefav = () => {
   const allItems = JSON.parse(localStorage.getItem("arrObject"));
+
+  const clearMovies = () => {
+    localStorage.removeItem("arrObject");
+    window.location.reload(false);
+  };
 
   return (
     <div className="movieTvFav">
       <h2>Movie Favorites!</h2>
       <div>
-        {allItems !== null ? allItems.map((item) => {
-          return (
-            <div className="results" key={item.id}>
-              <Link to={`/MoviePage/${item.id}`} className="linkName">
-                <img
-                  src={`https://image.tmdb.org/t/p/w92/` + item.img}
-                  alt={item.id}
-                />
-              </Link>
-              <div className="body">
-                <h3>{item.name}</h3>
-                <p>{item.overview}</p>
-              </div>
-            </div>
-          );
-        }) : <p style={{padding: '2%'}}>Nothing added yet. Go browse in our Movies section to add something!</p>}
+        {allItems !== null ? (
+          <button className="clear" onClick={clearMovies}>
+            Clear Movies
+          </button>
+        ) : null}
+        {allItems !== null ? (
+          allItems.map((item) => {
+            return (
+              <>
+                <div className="results" key={item.id}>
+                  <Link to={`/MoviePage/${item.id}`} className="linkName">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w92/` + item.img}
+                      alt={item.id}
+                    />
+                  </Link>
+                  <div className="body">
+                    <h3>{item.name}</h3>
+                    <p>{item.overview}</p>
+                  </div>
+                </div>
+              </>
+            );
+          })
+        ) : (
+          <p style={{ padding: "2%" }}>
+            Nothing added yet. Go browse in our Movies section to add something!
+          </p>
+        )}
       </div>
     </div>
   );
--- src/components/TvFav.js
+++ src/components/TvFav.js
@@ -4,26 +4,44 @@ import { Link } from "react-router-dom";
 const TvFav = () => {
   const allItems = JSON.parse(localStorage.getItem("arrObjectTv"));
 
+  const clearTv = () => {
+    localStorage.removeItem("arrObjectTv");
+    window.location.reload(false);
+  };
+
   return (
     <div className="movieTvFav">
       <h2>Tv Favorites!</h2>
       <div>
-        {allItems !== null ? allItems.map((item) => {
-          return (
-            <div className="results" key={item.id}>
-              <Link to={`/TvPage/${item.id}`} className="linkName">
-                <img
-                  src={`https://image.tmdb.org/t/p/w92/` + item.img}
-                  alt={item.id}
-                />
-              </Link>
-              <div className="body">
-                <h3>{item.name}</h3>
-                <p>{item.overview}</p>
-              </div>
-            </div>
-          );
-        }) : <p style={{padding: '2%'}}>Nothing added yet. Go browse in our Tv section to add something!</p>}
+        {allItems !== null ? (
+          <button className="clear" onClick={clearTv}>
+            Clear Tv
+          </button>
+        ) : null}
+        {allItems !== null ? (
+          allItems.map((item) => {
+            return (
+              <>
+                <div className="results" key={item.id}>
+                  <Link to={`/TvPage/${item.id}`} className="linkName">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w92/` + item.img}
+                      alt={item.id}
+                    />
+                  </Link>
+                  <div className="body">
+                    <h3>{item.name}</h3>
+                    <p>{item.overview}</p>
+                  </div>
+                </div>
+              </>
+            );
+          })
+        ) : (
+          <p style={{ padding: "2%" }}>
+            Nothing added yet. Go browse in our Tv section to add something!
+          </p>
+        )}
       </div>
     </div>
   );
