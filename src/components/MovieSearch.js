621060d79f8ae6984f3a35f7df5704df2b01fee5
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2022-12-22 22:16:22 -0700

    changes to search page

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaStar, FaRegHeart, FaHeart, FaSearch, FaVest } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { FreeMode, Pagination } from "swiper";
 import "swiper/css";
+import "swiper/css/free-mode";
 import "swiper/css/pagination";
-import "../App.css";
-
 const MovieSearch = () => {
   const [inputValue, setInputValue] = useState("");
   const [search, setSearch] = useState();
@@ -61,99 +60,60 @@ const MovieSearch = () => {
           </button>
         </form>
       </div>
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={10}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+          },
+        }}
+        modules={[FreeMode, Pagination]}
+      >
       {search
-        ?.filter((items, idx) => idx < 5)
+        ?.filter((items, idx) => idx < 10)
         .map((item) => {
           return (
-            <div className="card">
-              <Link to={`/MoviePage/${item.id}`} className="linkName">
-                <img
-                  src={`https://image.tmdb.org/t/p/w500/` + item?.poster_path}
-                  alt="No Image available"
-                />
-                <hr />
-                <h2>{item?.title}</h2>
-              </Link>
-              <ul className="top">
-                <li className="li1">
-                  {" "}
-                  <cite>Release Date: {item?.release_date}</cite>
-                </li>
-                <li className="li2">
-                  {" "}
-                  <cite className="rating">
-                    Rating: <FaStar /> {Math.round(item?.vote_average * 10)}%
-                  </cite>
-                </li>
-              </ul>
-              <h3>Overview</h3>
-              <p>{item?.overview}</p>
-              {/* <div className="whereToWatch">
-                <h4>Where to Stream...</h4>
-                <ul className="whereToWatchList">
-                  {stream?.length > 0
-                    ? stream?.map((item) => {
-                        return (
-                          <li key={item.provider_id} className="badge">
-                            {item.provider_name}
-                          </li>
-                        );
-                      })
-                    : "No streaming options available"}
-                  <cite
-                    style={{
-                      marginTop: "5%",
-                      marginLeft: "auto",
-                      color: "white",
-                    }}
-                  >
-                    Powered by JustWatch
-                  </cite>
-                </ul>
-              </div>
-              <div className="whereToBuy">
-                <h4>Where to buy...</h4>
-                <ul className="whereToBuyList">
-                  {buy?.length > 0
-                    ? buy?.map((item) => {
-                        return (
-                          <li key={item.provider_id} className="badge">
-                            {item.provider_name}
-                          </li>
-                        );
-                      })
-                    : "No purchase options available"}
-                  <cite
-                    style={{
-                      marginTop: "5%",
-                      marginLeft: "auto",
-                      color: "white",
-                    }}
-                  >
-                    Powered by JustWatch
-                  </cite>
-                </ul>
-              </div> */}
+            <div className="movie-page">
+              <SwiperSlide>
+                <div className="card">
+                  <Link to={`/MoviePage/${item.id}`} className="linkName">
+                    <img
+                      src={
+                        `https://image.tmdb.org/t/p/w500/` + item?.poster_path
+                      }
+                      alt="No Image available"
+                    />
+                  </Link>
+                </div>
+              </SwiperSlide>
             </div>
           );
         })}
-      <div className="recommended">
-        <Swiper pagination={true} modules={[Pagination]}>
+      {/* <div className="recommended">
+        <Swiper>
           {recommend
             ?.filter((items, idx) => idx < 5)
             .map((item) => {
               return (
-                <SwiperSlide style={{ paddingBottom: "5%", paddingTop: "10%" }}>
-                  <h3
-                    style={{
-                      textAlign: "center",
-                      marginBottom: "5%",
-                      padding: "0 2%",
-                    }}
-                  >
+                <SwiperSlide>
+                  <h3>
                     If you like {search?.[0].title} then you might like this.
                   </h3>
-                  <div className="card" style={{ height: "450px" }}>
+                  <div className="card">
                     <Link to={`/MoviePage/${item.id}`} className="linkName">
                       <img
                         src={
@@ -183,7 +143,8 @@ const MovieSearch = () => {
               );
             })}
         </Swiper>
-      </div>
+      </div> */}
+      </Swiper>
     </div>
   );
 };
