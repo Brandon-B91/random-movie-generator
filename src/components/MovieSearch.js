337ee553bc00ac9f110a71c4b1a6a6c7972a79b3
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2023-01-04 16:45:11 -0700

    removed some bugs

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -28,7 +28,7 @@ const MovieSearch = () => {
         let id = search[0].id;
         setId(id);
         setSearch(search);
-        localStorage.setItem('searchItem', id)
+        // localStorage.setItem('searchItem', id)
 
         return fetch(
           `https://api.themoviedb.org/3/movie/${id}?api_key=f79df266a37e366257a09e6b64a14de9&language=en-US&append_to_response=watch%2Fproviders,recommendations`
